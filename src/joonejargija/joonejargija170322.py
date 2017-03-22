42eb2ef8fbdc73fa701c17304cef0cea7bad6c2e
ojametsgh <mmhhxx@gmail.com>
2017-03-22 17:19:23 +0200

    Project with daisy projects

--- src/joonejargija/joonejargija170322.py
+++ src/joonejargija/joonejargija170322.py
@@ -1,4 +1,3 @@
-
 """
 - k2ib m00da musta joont, kui satub valge peale siis 
 """
@@ -6,33 +5,35 @@
 from ev3dev import ev3
 import time
 
+
 class Robot:
     def __init__(self):
-        self.speed = 800
+        self.speed = 400
+        self.btn = ev3.Button()
         self.motor_left = ev3.LargeMotor('outB')
         self.motor_right = ev3.LargeMotor('outC')
         self.color_sensor = ev3.ColorSensor('in2')
-        self.color_sensor.mode = "COL-REFLECT" # 0-100
+        self.color_sensor.mode = "COL-REFLECT"  # 0-100
 
     def drive(self):
-        self.motor_left.run_forever(speed_sp = self.speed)
-        self.motor_right.run_forever(speed_sp = self.speed)
+        self.motor_left.run_forever(speed_sp=self.speed)
+        self.motor_right.run_forever(speed_sp=self.speed)
 
     def turn(self, direction):
         if direction == "right":
             self.motor_right.stop()
-            self.motor_left.run_forever(speed_sp = self.speed)
+            self.motor_left.run_forever(speed_sp=self.speed * 2)
         elif direction == "left":
             self.motor_left.stop()
-            self.motor_right.run_forever(speed_sp = self.speed)
+            self.motor_right.run_forever(speed_sp=self.speed * 2)
 
     def turn_fast(self, direction):
         if direction == "right":
-            self.motor_right.run_forever(speed_sp = -self.speed)
-            self.motor_left.run_forever(speed_sp = self.speed)
+            self.motor_right.run_forever(speed_sp=-self.speed)
+            self.motor_left.run_forever(speed_sp=self.speed)
         elif direction == "left":
-            self.motor_left.run_forever(speed_sp = -self.speed)
-            self.motor_right.run_forever(speed_sp = self.speed)
+            self.motor_left.run_forever(speed_sp=-self.speed)
+            self.motor_right.run_forever(speed_sp=self.speed)
 
     def stop(self):
         self.motor_left.stop()
@@ -40,25 +41,14 @@ class Robot:
 
 
 def main():
+    robot = Robot()
     try:
-        btn = ev3.Button
-        while not btn.any:
-            robot = Robot()
-
+        while not robot.btn.any():
             robot.drive()
-            time.sleep(2)
-
-            robot.turn("right")
-            time.sleep(2)
-
-            robot.drive()
-            time.sleep(2)
-
-            robot.turn_fast("left")
-
-            robot.stop()
+        robot.stop()
     except KeyboardInterrupt:
         robot.stop()
+
 
 if __name__ == "__main__":
     main()
