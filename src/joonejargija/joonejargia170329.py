cc9ec649170033f4ab92f10254ab93e225c2711c
Ceisi <peik.ceisi@gmail.com>
2017-03-29 16:42:55 +0300

    Update joonejargia170329.py

--- src/joonejargija/joonejargia170329.py
+++ src/joonejargija/joonejargia170329.py
@@ -1,5 +1,7 @@
 """
-- k2ib m00da musta joont, kui satub valge peale siis 
+- soidab mustast joonest paremal
+- valge > 70
+- must < 11
 """
 
 from ev3dev import ev3
@@ -39,12 +41,65 @@ class Robot:
         self.motor_left.stop()
         self.motor_right.stop()
 
+    def sense_reflection(self):
+        return self.color_sensor.value()
+
+def isOnTrack(reflections, last_turn):
+    # reflections on list, kus 2 elementi
+    # 0 - straight (last - current = 0)
+    # 1 - right (last - current > 0)
+    # -1 - left (last - current < 0)
+    # 2 - fast right
+    # -2 - fast left
+    last = reflections[0]
+    current = reflections[1]
+
+    if (last == current) {
+        if (current > 45) {
+            if (last_turn = "right") {
+                return -1
+            } elif (last_turn = "left") {
+                return 2
+            }
+        }
+        return 0
+    } elif (last - current > 0) {
+        return 1
+    } elif (last - current < 0) {
+        return -1
+    }
 
 def main():
     robot = Robot()
+    last_turn = "right"
+
+    reflections = []
+            for x in range(0, 2):
+                reflections.extend(robot.sense_reflection())
+
     try:
         while not robot.btn.any():
-            print(robot.color_sensor.value())
+            robot.drive()
+            # lisab 3 iteratsiooni väärtused
+            reflections.extend(robot.sense_reflection())
+
+            # hoiab 2 iteratsiooni v22rtused (kustutab hilisema)
+            reflections.pop(0)
+
+            if (isOnTrack(reflections, last_turn) == 0) {
+                robot.drive()
+            } elif (isOnTrack(reflections, last_turn) == 1) {
+                last_turn = "right"
+                robot.turn_fast("right")
+            } elif (isOnTrack(reflections, last_turn) == -1) {
+                last_turn = "left"
+                robot.turn_fast("left")
+            } elif (isOnTrack(reflections, last_turn) == 2) {
+                last_turn("right")
+                robot.turn("right")
+            }
+
+
         robot.stop()
     except KeyboardInterrupt:
         robot.stop()
